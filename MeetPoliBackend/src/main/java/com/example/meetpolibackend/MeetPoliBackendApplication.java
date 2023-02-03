ae09c810d162405e5af2c780c1cf55e485d4f7ea
Sean Stevens <sstevens88@gmail.com>
2023-02-03 15:16:03 -0800

    added Spring project skeleton

---
+++ MeetPoliBackend/src/main/java/com/example/meetpolibackend/MeetPoliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetpolibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetPoliBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeetPoliBackendApplication.class, args);
+  }
+
+}
---
+++ MeetPoliBackend/src/test/java/com/example/meetpolibackend/MeetPoliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetpolibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetPoliBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
