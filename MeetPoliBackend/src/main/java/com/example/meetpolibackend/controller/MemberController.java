9296a90c8fe0566528b9b4d93978adc31930249f
Sean Stevens <sstevens88@gmail.com>
2023-02-04 12:22:17 -0800

    completed user timeline api endpoint

--- MeetPoliBackend/src/main/java/com/example/meetpolibackend/controller/MemberController.java
+++ MeetPoliBackend/src/main/java/com/example/meetpolibackend/controller/MemberController.java
@@ -1,12 +1,10 @@
 package com.example.meetpolibackend.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
